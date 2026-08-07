be5490610f31d5539fd9ab640b47d73e78608e01
WielkiGoggle <dekstery880@gmail.com>
2026-08-07 23:16:31 +0200

    Update callback.js

--- api/auth/discord/callback.js
+++ api/auth/discord/callback.js
@@ -1,23 +1,30 @@
+import { sql } from "@vercel/postgres";
+
 export default async function handler(req, res) {
+
   const code = req.query.code;
 
   if (!code) {
     return res.status(400).send("Brak kodu Discord");
   }
 
-  const response = await fetch("https://discord.com/api/oauth2/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      client_id: process.env.DISCORD_CLIENT_ID,
-      client_secret: process.env.DISCORD_CLIENT_SECRET,
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri: process.env.DISCORD_REDIRECT_URI,
-    }),
-  });
+  // Pobieranie tokena Discord
+  const response = await fetch(
+    "https://discord.com/api/oauth2/token",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: process.env.DISCORD_CLIENT_ID,
+        client_secret: process.env.DISCORD_CLIENT_SECRET,
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: process.env.DISCORD_REDIRECT_URI,
+      }),
+    }
+  );
 
   const data = await response.json();
 
@@ -25,6 +32,8 @@ export default async function handler(req, res) {
     return res.status(500).json(data);
   }
 
+
+  // Pobieranie użytkownika Discord
   const userResponse = await fetch(
     "https://discord.com/api/users/@me",
     {
@@ -36,10 +45,29 @@ export default async function handler(req, res) {
 
   const user = await userResponse.json();
 
+
+  // Zapis do bazy danych
+  await sql`
+    INSERT INTO users (
+      discord_id,
+      username,
+      avatar
+    )
+    VALUES (
+      ${user.id},
+      ${user.username},
+      ${user.avatar}
+    )
+    ON CONFLICT (discord_id)
+    DO UPDATE SET
+      username = ${user.username},
+      avatar = ${user.avatar};
+  `;
+
+
   res.status(200).json({
-    message: "Zalogowano!",
+    message: "Zalogowano i zapisano w bazie!",
     username: user.username,
-    avatar: user.avatar,
-    id: user.id,
+    id: user.id
   });
 }
