977e3c9e828d9f7f2cb4c89d42d9dbb013209887
WielkiGoggle <dekstery880@gmail.com>
2026-08-08 01:29:47 +0200

    Update user.js

--- api/auth/user.js
+++ api/auth/user.js
@@ -1,16 +1,28 @@
-export default function handler(req,res){
+export default async function handler(req, res) {
+  const cookies = req.headers.cookie || "";
 
-  if(!req.cookies || !req.cookies.discord_user){
-    return res.json({
-      loggedIn:false
+  const match = cookies.match(/(?:^|;\s*)discord_user=([^;]*)/);
+
+  if (!match) {
+    return res.status(200).json({
+      loggedIn: false
     });
   }
 
-  const user = JSON.parse(req.cookies.discord_user);
+  try {
+    const user = JSON.parse(decodeURIComponent(match[1]));
 
-  res.json({
-    loggedIn:true,
-    username:user.username,
-    avatar:`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-  });
+    return res.status(200).json({
+      loggedIn: true,
+      id: user.id,
+      username: user.username,
+      avatar: user.avatar
+        ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
+        : "https://cdn.discordapp.com/embed/avatars/0.png"
+    });
+  } catch (error) {
+    return res.status(200).json({
+      loggedIn: false
+    });
+  }
 }
