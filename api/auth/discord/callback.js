3b441eda8ed0caec6375ca2d86c0e1181ce9b544
WielkiGoggle <dekstery880@gmail.com>
2026-08-07 05:48:58 +0200

    Update callback.js

--- api/auth/discord/callback.js
+++ api/auth/discord/callback.js
@@ -1,3 +1,45 @@
-export default function handler(req, res) {
-  res.status(200).send("callback");
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  if (!code) {
+    return res.status(400).send("Brak kodu Discord");
+  }
+
+  const response = await fetch("https://discord.com/api/oauth2/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: process.env.DISCORD_CLIENT_ID,
+      client_secret: process.env.DISCORD_CLIENT_SECRET,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    }),
+  });
+
+  const data = await response.json();
+
+  if (!data.access_token) {
+    return res.status(500).json(data);
+  }
+
+  const userResponse = await fetch(
+    "https://discord.com/api/users/@me",
+    {
+      headers: {
+        Authorization: `Bearer ${data.access_token}`,
+      },
+    }
+  );
+
+  const user = await userResponse.json();
+
+  res.status(200).json({
+    message: "Zalogowano!",
+    username: user.username,
+    avatar: user.avatar,
+    id: user.id,
+  });
 }
