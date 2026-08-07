0cdc3677aca03b8647258c24caa959251ab7d235
WielkiGoggle <dekstery880@gmail.com>
2026-08-07 05:20:52 +0200

    Update login.js

--- api/auth/discord/login.js
+++ api/auth/discord/login.js
@@ -1,3 +1,13 @@
 export default function handler(req, res) {
-  res.status(200).send("login");
+  const clientId = process.env.DISCORD_CLIENT_ID;
+  const redirectUri = process.env.DISCORD_REDIRECT_URI;
+
+  const url =
+    "https://discord.com/oauth2/authorize" +
+    `?client_id=${clientId}` +
+    "&response_type=code" +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+    "&scope=identify";
+
+  res.redirect(url);
 }
