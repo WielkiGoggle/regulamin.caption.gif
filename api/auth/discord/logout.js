afbd1d2f96d7101f9b0ef919c4aeee8d2f3d748a
WielkiGoggle <dekstery880@gmail.com>
2026-08-08 03:44:37 +0200

    Update logout.js

--- api/auth/discord/logout.js
+++ api/auth/discord/logout.js
@@ -1,3 +1,10 @@
-export default function handler(req, res) {
-  res.redirect("/");
+export default async function handler(req, res) {
+  res.setHeader("Set-Cookie", [
+    "discord_user=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax"
+  ]);
+
+  res.writeHead(302, {
+    Location: "/"
+  });
+  res.end();
 }
