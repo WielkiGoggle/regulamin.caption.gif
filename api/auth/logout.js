d9c89c093fe6d1eff73e00486f42e62cccd7ce06
WielkiGoggle <dekstery880@gmail.com>
2026-08-08 03:35:40 +0200

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -4,7 +4,5 @@ export default async function handler(req, res) {
     "discord_user=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax"
   );
 
-  return res.status(200).json({
-    success: true
-  });
+  return res.redirect(302, "/");
 }
