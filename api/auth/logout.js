35bf7a037625b270fef23613d5e5bd9d6a2f3cbf
WielkiGoggle <dekstery880@gmail.com>
2026-08-08 03:09:45 +0200

    Update logout.js

--- api/auth/logout.js
+++ api/auth/logout.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   res.setHeader(
     "Set-Cookie",
-    "discord_user=; Path=/; Max-Age=0; SameSite=Lax"
+    "discord_user=; Path=/; Max-Age=0; HttpOnly; Secure; SameSite=Lax"
   );
 
   return res.status(200).json({
