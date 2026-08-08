9001a295516a86bd3ea2bb3fcbc64d6749482646
WielkiGoggle <dekstery880@gmail.com>
2026-08-08 02:26:16 +0200

    Update user.js

--- api/auth/user.js
+++ api/auth/user.js
@@ -12,13 +12,17 @@ export default async function handler(req, res) {
   try {
     const user = JSON.parse(decodeURIComponent(match[1]));
 
+    let avatar = "https://cdn.discordapp.com/embed/avatars/0.png";
+
+    if (user.avatar) {
+      avatar = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+    }
+
     return res.status(200).json({
       loggedIn: true,
       id: user.id,
       username: user.username,
-      avatar: user.avatar
-        ? `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`
-        : "https://cdn.discordapp.com/embed/avatars/0.png"
+      avatar: avatar
     });
   } catch (error) {
     return res.status(200).json({
